80397f8760de7d4e43004ac539084b97f3edb668
dominic.stalder <dominic.stalder89@gmail.com>
2022-12-10 13:24:48 +0100

    r fix tests

--- aquabasilea-kurs-bucher-rest-app/src/test/java/com/aquabasilea/rest/model/course/WeeklyCoursesDtoTest.java
+++ aquabasilea-kurs-bucher-rest-app/src/test/java/com/aquabasilea/rest/model/course/WeeklyCoursesDtoTest.java
@@ -7,6 +7,8 @@ import com.aquabasilea.rest.model.course.weeklycourses.WeeklyCoursesDto;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
@@ -19,11 +21,11 @@ class WeeklyCoursesDtoTest {
    void getWeeklyCoursesDto() {
 
       // Given
-      String secondCourseId = "1";
-      String thirdCourseId = "2";
-      String fourthCourseId = "3";
-      String firstCourseId = "4";
-      LocalDateTime firstCourseDate = LocalDateTime.now()
+      String firstCourseId = "1";
+      String secondCourseId = "2";
+      String thirdCourseId = "3";
+      String lastCourseId = "4";
+      LocalDateTime firstCourseDate = LocalDateTime.of(2022, 12, 12, 12, 15)
               .plusDays(1);
       LocalDateTime secondCourseDate = firstCourseDate
               .plusMinutes(10);
@@ -35,21 +37,88 @@ class WeeklyCoursesDtoTest {
       Course currentCourse = CourseBuilder.builder()
               .withCourseDate(thirdCourseDate)
               .withCourseName("Kurs-abc1")
-              .withId(fourthCourseId)
+              .withId(lastCourseId)
               .build();
-      weeklyCourses.setCourses(List.of(CourseBuilder.builder()
+      List<Course> courses = new ArrayList<>(List.of(CourseBuilder.builder()
               .withCourseDate(secondCourseDate)
               .withCourseName("Kurs-abc2")
-              .withId(thirdCourseId)
+              .withId(secondCourseId)
               .build(), currentCourse, CourseBuilder.builder()
               .withCourseDate(firstCourseDate)
               .withCourseName("Kurs-abc3")
-              .withId(secondCourseId)
+              .withId(firstCourseId)
               .build(), CourseBuilder.builder()
               .withCourseDate(fourthCourseDate)
               .withCourseName("Kurs-abc3")
-              .withId(firstCourseId)
+              .withId(thirdCourseId)
               .build()));
+      Collections.shuffle(courses);
+      weeklyCourses.setCourses(courses);
+
+      // When
+      WeeklyCoursesDto weeklyCoursesDto = WeeklyCoursesDto.of(weeklyCourses, currentCourse, Locale.GERMAN);
+
+      // Then
+      assertThat(weeklyCoursesDto.courseDtos().get(0).id(), is(firstCourseId));
+      assertThat(weeklyCoursesDto.courseDtos().get(1).id(), is(secondCourseId));
+      assertThat(weeklyCoursesDto.courseDtos().get(2).id(), is(thirdCourseId));
+      assertThat(weeklyCoursesDto.courseDtos().get(3).id(), is(lastCourseId));
+      assertThat(weeklyCoursesDto.courseDtos().get(3).isCurrentCourse(), is(true));
+   }
+
+   @Test
+   void getWeeklyCoursesDto2() {
+
+      // Given
+      String firstCourseId = "1";
+      String secondCourseId = "2";
+      String thirdCourseId = "3";
+      String fourthCourseId = "4";
+      String fifthCourseId = "5";
+      String sixthCourseId = "6";
+      LocalDateTime firstCourseDate = LocalDateTime.of(2022,12,12, 18,15,0);
+      LocalDateTime secondCourseDate = firstCourseDate
+              .plusMinutes(30);
+      LocalDateTime thirdCourseDate = firstCourseDate
+              .plusDays(2);
+      LocalDateTime fourthCourseDate = thirdCourseDate
+              .plusDays(1)
+              .minusHours(8);
+      LocalDateTime fifthCourseDate = thirdCourseDate
+              .plusDays(1)
+              .plusHours(1);
+      LocalDateTime sixthCourseDate = thirdCourseDate
+              .plusDays(2)
+              .minusHours(6);
+      WeeklyCourses weeklyCourses = new WeeklyCourses();
+      Course currentCourse = CourseBuilder.builder()
+              .withCourseDate(thirdCourseDate)
+              .withCourseName("Kurs-abc1")
+              .withId(thirdCourseId)
+              .build();
+      List<Course> courses = new ArrayList<>(List.of(CourseBuilder.builder()
+              .withCourseDate(secondCourseDate)
+              .withCourseName("Kurs-abc2")
+              .withId(secondCourseId)
+              .build(), currentCourse, CourseBuilder.builder()
+              .withCourseDate(firstCourseDate)
+              .withCourseName("Kurs-abc3")
+              .withId(firstCourseId)
+              .build(), CourseBuilder.builder()
+              .withCourseDate(fourthCourseDate)
+              .withCourseName("Kurs-abc4")
+              .withId(fourthCourseId)
+              .build(), CourseBuilder.builder()
+              .withCourseDate(fifthCourseDate)
+              .withCourseName("Kurs-5")
+              .withId(fifthCourseId)
+              .build(), CourseBuilder.builder()
+              .withCourseDate(sixthCourseDate)
+              .withCourseName("Kurs-6")
+              .withId(sixthCourseId)
+              .build()));
+      Collections.shuffle(courses);
+      weeklyCourses.setCourses(courses);
 
       // When
       WeeklyCoursesDto weeklyCoursesDto = WeeklyCoursesDto.of(weeklyCourses, currentCourse, Locale.GERMAN);
@@ -59,6 +128,8 @@ class WeeklyCoursesDtoTest {
       assertThat(weeklyCoursesDto.courseDtos().get(1).id(), is(secondCourseId));
       assertThat(weeklyCoursesDto.courseDtos().get(2).id(), is(thirdCourseId));
       assertThat(weeklyCoursesDto.courseDtos().get(3).id(), is(fourthCourseId));
-      assertThat(weeklyCoursesDto.courseDtos().get(3).isCurrentCourse(), is(true));
+      assertThat(weeklyCoursesDto.courseDtos().get(4).id(), is(fifthCourseId));
+      assertThat(weeklyCoursesDto.courseDtos().get(5).id(), is(sixthCourseId));
+      assertThat(weeklyCoursesDto.courseDtos().get(2).isCurrentCourse(), is(true));
    }
 }
--- aquabasilea-kurs-bucher/src/main/java/com/aquabasilea/model/course/weeklycourses/CourseComparator.java
+++ aquabasilea-kurs-bucher/src/main/java/com/aquabasilea/model/course/weeklycourses/CourseComparator.java
@@ -11,11 +11,11 @@ public class CourseComparator implements Comparator<Course> {
       // and only if both weekdays are same, compare the entire date
       if (courseDate1.getDayOfWeek() == courseDate2.getDayOfWeek()) {
          // but switch courseDate so the current TUESDAY (e.q. 5.7) comes after the futur TUESDAY (12.7) -> chronological order
-         int compareLocalDate = courseDate1.toLocalDate().compareTo(courseDate2.toLocalDate());
+         int compareLocalDate = courseDate1.compareTo(courseDate2);
          if (compareLocalDate == 0) {
             return courseDate1.toLocalTime().compareTo(courseDate2.toLocalTime());
          }
-         return courseDate2.compareTo(courseDate1);
+         return compareLocalDate;
       }
       // Just compare the week days, in order to get an order like 'monday', 'tuesday'..
       return courseDate1.getDayOfWeek().compareTo(courseDate2.getDayOfWeek());
