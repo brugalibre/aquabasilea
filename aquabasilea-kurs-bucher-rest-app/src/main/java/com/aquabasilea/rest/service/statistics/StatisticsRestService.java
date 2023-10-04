139108d7cd91bd3d3c12bed61bc393376356cf99
dominic.stalder <dominic.stalder89@gmail.com>
2023-10-04 08:37:31 +0200

    b fix wrong calculation of uptime in days

--- aquabasilea-kurs-bucher-rest-app/src/main/java/com/aquabasilea/rest/service/statistics/StatisticsRestService.java
+++ aquabasilea-kurs-bucher-rest-app/src/main/java/com/aquabasilea/rest/service/statistics/StatisticsRestService.java
@@ -4,12 +4,15 @@ import com.aquabasilea.application.i18n.TextResources;
 import com.aquabasilea.rest.i18n.LocaleProvider;
 import com.aquabasilea.rest.model.statistic.StatisticsDto;
 import com.aquabasilea.service.statistics.StatisticsService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.RuntimeMXBean;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.Period;
 
 @Service
 public class StatisticsRestService {
@@ -20,9 +23,13 @@ public class StatisticsRestService {
 
    @Autowired
    public StatisticsRestService(StatisticsService statisticsService, LocaleProvider localeProvider) {
+      this(statisticsService, localeProvider, ManagementFactory.getRuntimeMXBean());
+   }
+
+   StatisticsRestService(StatisticsService statisticsService, LocaleProvider localeProvider, RuntimeMXBean runtimeMXBean) {
       this.localeProvider = localeProvider;
       this.statisticsService = statisticsService;
-      this.runtimeMXBean = ManagementFactory.getRuntimeMXBean();
+      this.runtimeMXBean = runtimeMXBean;
    }
 
    public StatisticsDto getStatisticDtoByUserId(String userId) {
@@ -30,16 +37,21 @@ public class StatisticsRestService {
    }
 
    public String getDurationString() {
+      return getDurationRelative(LocalDateTime.now());
+   }
+
+   String getDurationRelative(LocalDateTime now) {
       Duration uptimeDuration = getUptimeDuration();
-      long totalDays = uptimeDuration.toDays();
-      long years = Math.floorDiv(totalDays, 365);
-      long restDays = totalDays - (years * 365);
-      long month = Math.floorDiv(restDays, 31);
+      LocalDateTime startTime = now.minusNanos(uptimeDuration.toNanos());
+      Period between = Period.between(startTime.toLocalDate(), now.toLocalDate());
+      long years = between.getYears();
+      long days = between.getDays();
+      long month = between.getMonths();
       String yearsAndMonth = "";
       if (years > 0 || month > 0) {
          yearsAndMonth = TextResources.UPTIME_YEARS_AND_MONTH.formatted(years, month) + ", ";
       }
-      return yearsAndMonth + TextResources.UPTIME_DAYS_HOURS_MINUTES.formatted(uptimeDuration.toDaysPart(), uptimeDuration.toHoursPart(), uptimeDuration.toMinutesPart());
+      return yearsAndMonth + TextResources.UPTIME_DAYS_HOURS_MINUTES.formatted(days, uptimeDuration.toHoursPart(), uptimeDuration.toMinutesPart());
    }
 
    public Duration getUptimeDuration() {
--- aquabasilea-kurs-bucher-rest-app/src/test/java/com/aquabasilea/rest/service/statistics/StatisticsRestServiceTest.java
+++ aquabasilea-kurs-bucher-rest-app/src/test/java/com/aquabasilea/rest/service/statistics/StatisticsRestServiceTest.java
@@ -1,10 +1,11 @@
-package com.aquabasilea.rest.service;
+package com.aquabasilea.rest.service.statistics;
 
 import com.aquabasilea.domain.statistics.model.Statistics;
 import com.aquabasilea.domain.statistics.model.repository.StatisticsRepository;
 import com.aquabasilea.rest.config.TestAquabasileaCourseBookerRestAppConfig;
+import com.aquabasilea.rest.i18n.LocaleProvider;
 import com.aquabasilea.rest.model.statistic.StatisticsDto;
-import com.aquabasilea.rest.service.statistics.StatisticsRestService;
+import com.aquabasilea.service.statistics.StatisticsService;
 import com.brugalibre.domain.contactpoint.mobilephone.model.MobilePhone;
 import com.brugalibre.domain.user.model.User;
 import com.brugalibre.domain.user.repository.UserRepository;
@@ -14,8 +15,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.lang.management.RuntimeMXBean;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = {TestAquabasileaCourseBookerRestAppConfig.class})
 class StatisticsRestServiceTest {
@@ -53,5 +62,26 @@ class StatisticsRestServiceTest {
 
       // Then
       assertThat(statisticDto.bookingSuccessRate(), is(100.0));
+   }
+
+   @Test
+   void testGetDurationString() {
+      // Given
+      LocalDateTime now = LocalDateTime.of(LocalDate.of(2023,10,4), LocalTime.of(8,35));
+      Duration uptime = Duration.ofDays(401).plusHours(2).plusMinutes(15);
+      RuntimeMXBean runtimeMXBean = getRuntimeMXBean(uptime);
+      StatisticsRestService statisticsRestService = new StatisticsRestService(mock(StatisticsService.class), new LocaleProvider(), runtimeMXBean);
+
+      // When
+      String durationString = statisticsRestService.getDurationRelative(now);
+
+      // Then
+      assertThat(durationString, is("1 Jahre, 1 Monate, 5 Tage, 2 Stunden, 15 Minuten"));
+   }
+
+   private static RuntimeMXBean getRuntimeMXBean(Duration uptime) {
+      RuntimeMXBean runtimeMXBean = mock(RuntimeMXBean.class);
+      when(runtimeMXBean.getUptime()).thenReturn(uptime.toMillis());
+      return runtimeMXBean;
    }
 }
