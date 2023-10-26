5670ecea40ff8567b471f236d68a9bcadde4f16b
dominic.stalder <dominic.stalder89@gmail.com>
2023-10-26 10:22:43 +0200

    f redirect to login if the user is not authenticated for a specific url

--- aquabasilea-kurs-bucher-web-ui/src/main/ui/src/router.js
+++ aquabasilea-kurs-bucher-web-ui/src/main/ui/src/router.js
@@ -43,9 +43,12 @@ router.beforeEach((to, from, next) => {
     const loggedIn = localStorage.getItem('user');
     // trying to access a restricted page + not logged in
     // redirect to login page
+    console.log('Coming from ' + from.path  + ' and going to ' + to.path);
     if (authRequired && !loggedIn) {
+        console.log('Redirect to login page');
         next(RouterConstants.LOGIN_PATH);
     } else {
+        console.log('Next');
         next();
     }
 });
